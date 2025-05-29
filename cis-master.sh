79f66b803cf672955d98ac82669cd4502d675f0a
Sivaneni <sivaneni1992@gmail.com>
2025-05-29 19:43:21 +0530

    kube-bench testing

--- cis-master.sh
+++ cis-master.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #cis-master.sh
 
-total_fail=$(kube-bench master  --version 1.15 --check 1.2.7,1.2.8,1.2.9 --json | jq .[].total_fail)
-
+#total_fail=$(kube-bench master  --version 1.15 --check 1.2.7,1.2.8,1.2.9 --json | jq .[].total_fail)
+total_fail=$(kube-bench run master  --check 1.2.7,1.2.8,1.2.9 --json | jq .Totals.total_fail)
 if [[ "$total_fail" -ne 0 ]];
         then
                 echo "CIS Benchmark Failed MASTER while testing for 1.2.7, 1.2.8, 1.2.9"
